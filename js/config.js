77f0354f4a1c33a387aa276de75063d0c4201f63
w731124 <w731124@gmail.com>
2026-09-03 11:52:53 +0800

    Wire frontend to deployed GAS Web App URL

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // 部署新版 GAS Web App 後只需更新這裡，不用改 api.js / app.js
 window.GAS_CONFIG = {
-  URL: 'PASTE_YOUR_GAS_WEB_APP_URL_HERE'
+  URL: 'https://script.google.com/macros/s/AKfycbyB2Og9aZEl33HMxMXWj8qgFLeRqdISCvt4_F5vM61kBZgTBAu8oLMNc8pmfWOVZ9A8Qw/exec'
 };
